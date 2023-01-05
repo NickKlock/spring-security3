8e5201d3c73bdcef5922df699a32437ecf6752e1
nick <klockgether.nick@gmail.com>
2023-01-05 17:23:37 +0100

    feat: Security with csrf and jsesssion token

--- src/main/java/com/hextrix/springsecurity3/SecurityConfig.java
+++ src/main/java/com/hextrix/springsecurity3/SecurityConfig.java
@@ -2,27 +2,43 @@ package com.hextrix.springsecurity3;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestHandler;
+import org.springframework.security.web.csrf.XorCsrfTokenRequestAttributeHandler;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+
     @Bean
     public SecurityFilterChain securityFilterChain(
-        HttpSecurity httpSecurity
+            HttpSecurity httpSecurity
     ) throws Exception {
+        CookieCsrfTokenRepository tokenRepository = CookieCsrfTokenRepository.withHttpOnlyFalse();
+        XorCsrfTokenRequestAttributeHandler delegate = new XorCsrfTokenRequestAttributeHandler();
+        delegate.setCsrfRequestAttributeName("_csrf");
+        CsrfTokenRequestHandler requestHandler = delegate::handle;
+
         return httpSecurity
-            .csrf().disable()
-            .httpBasic()
-            .and()
-            .authorizeHttpRequests()
-            .requestMatchers("/admin-only").hasRole("ADMIN")
-            .requestMatchers("/**").permitAll()
-            .anyRequest()
-            .authenticated()
-            .and()
-            .build();
+                .csrf(csrf -> csrf
+                        .csrfTokenRepository(tokenRepository)
+                        .csrfTokenRequestHandler(requestHandler))
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                .httpBasic()
+                .and()
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST,"/login").permitAll()
+                        .requestMatchers("/any-endpoint").permitAll()
+                        .requestMatchers("/admin-only").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .build();
     }
 }
