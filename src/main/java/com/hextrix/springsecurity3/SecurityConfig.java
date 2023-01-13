f8810ea7be604f5b7332a1fd5086071905638ab1
nick <klockgether.nick@gmail.com>
2023-01-13 12:10:22 +0100

    Update: SecurityConfig to behave like in Spring-Security 5.8

--- src/main/java/com/hextrix/springsecurity3/SecurityConfig.java
+++ src/main/java/com/hextrix/springsecurity3/SecurityConfig.java
@@ -3,13 +3,15 @@ package com.hextrix.springsecurity3;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.csrf.CsrfTokenRequestHandler;
-import org.springframework.security.web.csrf.XorCsrfTokenRequestAttributeHandler;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+
 
 @Configuration
 @EnableWebSecurity
@@ -17,28 +19,33 @@ public class SecurityConfig {
 
 
     @Bean
-    public SecurityFilterChain securityFilterChain(
-            HttpSecurity httpSecurity
-    ) throws Exception {
-        CookieCsrfTokenRepository tokenRepository = CookieCsrfTokenRepository.withHttpOnlyFalse();
-        XorCsrfTokenRequestAttributeHandler delegate = new XorCsrfTokenRequestAttributeHandler();
-        delegate.setCsrfRequestAttributeName("_csrf");
-        CsrfTokenRequestHandler requestHandler = delegate::handle;
+    public SecurityFilterChain securityFilterChain( HttpSecurity httpSecurity ) throws Exception {
+        //Set the CsrfTokenRequestAttributeHandler to 5.8 default
+        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
+        requestHandler.setCsrfRequestAttributeName(null);
 
         return httpSecurity
                 .csrf(csrf -> csrf
-                        .csrfTokenRepository(tokenRepository)
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                         .csrfTokenRequestHandler(requestHandler))
+                //JSESSIONID on every request
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
                 .httpBasic()
                 .and()
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.POST,"/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/login").permitAll()
                         .requestMatchers("/any-endpoint").permitAll()
                         .requestMatchers("/admin-only").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
+                //POST request on the defined endpoint will logout the current user.
+                .logout(logout -> logout
+                        .logoutUrl("/api/users/logout")
+                        .clearAuthentication(true)
+                        .invalidateHttpSession(true)
+                        .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler(HttpStatus.OK))
+                        .permitAll())
                 .build();
     }
 }
